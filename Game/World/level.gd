4441d47a1016caf8b1285332d5e58a204eed56b1
Alex Cristoffanini <acristoffanini@gmail.com>
2025-04-12 12:11:03 -0700

    add goblin to game scene

--- Game/World/level.gd
+++ Game/World/level.gd
@@ -7,4 +7,6 @@ func _ready():
 	for civvies in civilian_count:
 		var new_pip = Pip.new_pip("normal")
 		add_child(new_pip)
+	var new_goblin = Pip.new_pip(goblin_type)
+	add_child(new_goblin)
 	pass
