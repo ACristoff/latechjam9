10c007fad13feaab8e59b3b0e7c20c9ebdc21f3d
Alex Cristoffanini <acristoffanini@gmail.com>
2025-04-12 11:49:13 -0700

    pip bounce logic

--- Game/Pip/base_pip.gd
+++ Game/Pip/base_pip.gd
@@ -1,7 +1,8 @@
-class_name Pip extends Node2D
+class_name Pip extends CharacterBody2D
 
 const PIP_SCENE: PackedScene = preload("res://Game/Pip/base_pip.tscn")
 
+@export var speed = 50
 
 
 static func new_pip(type: String) -> Pip:
@@ -11,5 +12,20 @@ static func new_pip(type: String) -> Pip:
 
 
 
-func _ready():
-	print('hallo')
+#func _ready():
+	##print('hallo')
+	#pass
+
+func start(_position, _direction):
+	rotation = _direction
+	position = _position
+	velocity = Vector2(speed, 0).rotated(rotation)
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+		if collision.get_collider().has_method("hit"):
+			collision.get_collider().hit()
+	
+	pass
