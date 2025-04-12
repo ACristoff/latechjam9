de4c24a70b1053aecc74119d03585278717d5fc7
Alex Cristoffanini <acristoffanini@gmail.com>
2025-04-12 11:34:33 -0700

    pip planning

--- Game/Pip/base_pip.gd
+++ Game/Pip/base_pip.gd
@@ -1,7 +1,14 @@
 class_name Pip extends Node2D
 
+const PIP_SCENE: PackedScene = preload("res://Game/Pip/base_pip.tscn")
+
+
+
 func new_pip(type: String) -> void:
 	print(type)
+	var new_pip: Pip = PIP_SCENE.instantiate()
+
+
 
 func _ready():
 	print('hallo')
