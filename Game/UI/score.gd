cfdd51bf6ef97a93b5d1347bf95355d956144a38
kinteyoi <calebazlin5@gmail.com>
2025-04-12 16:25:15 -0500

    score

--- Game/UI/score.gd
+++ Game/UI/score.gd
@@ -9,11 +9,11 @@ func _ready() -> void:
 	var randscale = randi_range(1, 2)
 	if randscale == 1:
 		print("true")
-		goblin_head.scale = Vector2(-2, 2)
+		goblin_head.scale = Vector2(-.145, .145)
 		collision_polygon_2d.scale = Vector2(-1, 1)
 		ticker = false
 	else:
 		print("false")
-		goblin_head.scale = Vector2(2, 2)
+		goblin_head.scale = Vector2(.145, .145)
 		collision_polygon_2d.scale = Vector2(1, 1)
 		ticker = true
