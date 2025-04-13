705ce76337cbbb9dbdc7f428864a4c2128178cac
kinteyoi <calebazlin5@gmail.com>
2025-04-13 13:49:33 -0500

    stopwatchgdscene

---
+++ Game/UI/stop_watch.gd
@@ -0,0 +1,24 @@
+extends Node2D
+@onready var timer: Timer = $Timer
+@onready var stop_watch: Sprite2D = $StopWatch
+@onready var timelab: Label = $Time
+@onready var wiggle: AnimationPlayer = $Wiggle
+
+var time = 30
+func _ready() -> void:
+	timelab.text = str(time)
+	start()
+
+func start():
+	timer.start()
+
+func _process(delta: float) -> void:
+	if time <= 0:
+		timer.stop
+		print("lose con")
+		print(" call dont have it in eyes overlay")
+
+func _on_timer_timeout() -> void:
+	wiggle.play("wiggle")
+	time -= 1
+	timelab.text = str(time)
